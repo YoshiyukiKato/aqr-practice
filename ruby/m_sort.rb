1b10137e41160dbbed3060722ca7c2480334e9a5
Yoshiyuki Kato <yo.shuuuuki@gmail.com>
2016-05-15 02:08:54 +0900

    fix comment

--- ruby/m_sort.rb
+++ ruby/m_sort.rb
@@ -26,7 +26,6 @@ def select_kth_tr(ar, k, left, right)
     # anchorの方が大きいなら、探索範囲を右側から狭める必要がある
     # [left...left+k-1...anchor...right]
     # -> [left...left+k-1...anchor-1]
-    # [left...left+k-1...anchor-1]
     next_k = k
     next_left = left
     next_right = anchor - 1
@@ -34,8 +33,7 @@ def select_kth_tr(ar, k, left, right)
   else
     # anchorの方が小さいなら、探索範囲を左側から狭める必要がある
     # [left...anchor...left+k-1...right]
-    # -> [anchor+1...(k+left)...right]
-    # -> [anchor+1...(k+left-1)-anchor...right]
+    # -> [anchor+1...k+left-1...right]
     next_k = (k + left - 1) - anchor
     next_left = anchor + 1
     next_right = right
