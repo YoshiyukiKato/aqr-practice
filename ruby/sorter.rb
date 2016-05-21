23f90a242a9a85436da0e0036583bc1e09d0aad7
Yoshiyuki Kato <yo.shuuuuki@gmail.com>
2016-05-21 15:14:55 +0900

    modify sample array generator

--- ruby/sorter.rb
+++ ruby/sorter.rb
@@ -2,7 +2,7 @@ require 'benchmark'
 
 module Sorter
   def self.run(is_debug=false, sample_num=100000)
-    ar = (1..sample_num).to_a.shuffle 
+    ar = (0...sample_num).to_a.shuffle 
     left = 0
     right = ar.length - 1 
     
